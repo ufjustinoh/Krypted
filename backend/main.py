81da998e41567b403e96d4f01304a6e30012cbca
Justin Oh <justinoh1227@gmail.com>
2026-05-06 23:13:09 -0400

    Add all Vercel URLs to CORS allowlist

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,9 @@ app.add_middleware(
     CORSMiddleware,                                                 # this tells backend its ok to accept requests from localhost:5172 (react app) otherwise browser would block this
     allow_origins = [
         "http://localhost:5173",
+        "https://password-manager-five-lake.vercel.app",
         "https://password-manager-80ea9wmfo-ufjustinohs-projects.vercel.app",
+        "https://password-manager-gvuxpw3b5-ufjustinohs-projects.vercel.app",
     ],
     allow_credentials = True,
     allow_methods = ["*"],
